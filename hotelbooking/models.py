ebb827efc993c5cf4965813e9ec4c7995e93d812
tejas2008 <tejas.khanolkar@somaiya.edu>
2020-11-21 12:53:49 +0530

    models addded

--- hotelbooking/models.py
+++ hotelbooking/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 # Create your models here.
-<<<<<<< HEAD
 class Exam(models.Model):
     Question = models.CharField(max_length = 100)
     Option1 = models.CharField(max_length = 100)
@@ -9,6 +8,4 @@ class Exam(models.Model):
     Option3 = models.CharField(max_length = 100)
     Option4 = models.CharField(max_length = 100)
     Corrans = models.CharField(max_length = 100)
-=======
->>>>>>> 99b837ae9e983e21ae29389e93024aab004edf3c
 
