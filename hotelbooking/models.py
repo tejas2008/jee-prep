63a1ad36a6b57219a532fc6a94931cad138b6315
tejas2008 <tejas.khanolkar@somaiya.edu>
2020-11-21 16:39:07 +0530

    quiz validation added

--- hotelbooking/models.py
+++ hotelbooking/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Exam(models.Model):
     Question = models.CharField(max_length = 100)
+    Qid = models.CharField(max_length=100)
     Option1 = models.CharField(max_length = 100)
     Option2 = models.CharField(max_length = 100)
     Option3 = models.CharField(max_length = 100)
--- hotelbooking/views.py
+++ hotelbooking/views.py
@@ -10,12 +10,29 @@ def index(request):
     return render(request, 'hotelbooking1/index.html',{'main_link':'http://127.0.0.1:8000/main/','adv_link':'http://127.0.0.1:8000/advance/'})
 
 def quiz(request):
-    results=Exam.objects.all()
-    return render(request, 'hotelbooking1/quiz.html',{"Exam":results})
+    if request.POST:
+        marks = 0
+        for i in range(3):
+            ans = request.POST['radio'+str(i+1)]
+            correct_ans = Exam.objects.filter(Qid=str(i+1))[0].Corrans
+            print(correct_ans)
+            print(ans)
+            if ans==correct_ans:
+                marks += 4
+            else:
+                marks -= 1
+        print(marks)
+        results=Exam.objects.all()
+        return render(request,'hotelbooking1/marks.html',{"Exam":results,"marks":marks})
+    else:
+        results=Exam.objects.all()
+        return render(request, 'hotelbooking1/quiz.html',{"Exam":results})
 
 def main(request):
     return render(request, 'hotelbooking1/main.html',{'quiz_link':'http://127.0.0.1:8000/quiz/'})
 
 def advance(request):
     return render(request, 'hotelbooking1/advance.html',{'quiz_link':'http://127.0.0.1:8000/quiz/'})
+
+
 
