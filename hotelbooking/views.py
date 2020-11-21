cf5c3eef9370803f8a5320f9e1357d6102db8073
Debdatta123 <debdatta.k@somaiya.edu>
2020-11-21 12:49:49 +0530

    added templating

--- hotelbooking/views.py
+++ hotelbooking/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,HttpResponse
 import datetime
 from datetime import date
+from hotelbooking.models import Exam
 
 # Create your views here.
 # def Hello1(request):
@@ -9,7 +10,8 @@ def index(request):
     return render(request, 'hotelbooking1/index.html',{'main_link':'http://127.0.0.1:8000/main/','adv_link':'http://127.0.0.1:8000/advance/'})
 
 def quiz(request):
-    return render(request, 'hotelbooking1/quiz.html')
+    results=Exam.objects.all()
+    return render(request, 'hotelbooking1/quiz.html',{"Exam":results})
 
 def main(request):
     return render(request, 'hotelbooking1/main.html',{'quiz_link':'http://127.0.0.1:8000/quiz/'})
