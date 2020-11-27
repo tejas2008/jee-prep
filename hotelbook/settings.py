eff34feb40f8c0499f8c37391dc9fd075876767d
tejas2008 <tejas.khanolkar@somaiya.edu>
2020-11-27 15:45:49 +0530

    update database

--- hotelbook/settings.py
+++ hotelbook/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
 }
 
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['defualt'].update(db_from_env)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
