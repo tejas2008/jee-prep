3c204975c9bd094560578e2f3cc24514193301b6
tejas2008 <tejas.khanolkar@somaiya.edu>
2020-11-27 15:48:38 +0530

    update database

--- hotelbook/settings.py
+++ hotelbook/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhitenoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'hotelbook.urls'
